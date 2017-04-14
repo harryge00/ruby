d58911f3e1452600005991e882361d396d155cf6
haoyuan <haoyuan.ge@caicloud.io>
2017-04-14 14:40:30 +0800

    proc and closure

--- Wagon-2/09. Storing blocks in Proc/love_message.rb
+++ Wagon-2/09. Storing blocks in Proc/love_message.rb
@@ -1,10 +1,13 @@
 def tell(who, &message_block)
   # A new Proc object is created from the given message_block 
   # Your code goes here...
+  puts who + ", " + message_block.call
+  # puts message_block.call
 end
 
 def tell_mum(&message_block)
   #...and here (you should call the tell method of course)
+  tell("mum", &message_block)
 end
 
 tell_mum {"I love you"} # => "mum, I love you !"
@@ -14,10 +17,12 @@ tell_mum {"I love you"} # => "mum, I love you !"
 def tell(who, message_blk)
   # The Proc object is created outside the method and passed as normal parameter
   # Your code goes here...
+  puts who + ", " + message_blk.call
 end
 
 def tell_mum(message_blk)
   #...and here (you should call the tell method of course)
+  tell("mum", message_blk)
 end
 
 love_block = Proc.new {"I love you"}
--- Wagon-2/10. Blocks are closures/welcome_closure.rb
+++ Wagon-2/10. Blocks are closures/welcome_closure.rb
@@ -1,10 +1,17 @@
 count = 0
 
-welcome_blk = Proc.new do |name| 
+def welcome_func
+  array = []
   # your code goes here !
+  return Proc.new do |name|
+	  array << name
+	  p 'Welcome ' + name + ', join your ' + (array.size() - 1).to_s + ' friend'
+	end
 end
 
+welcome_blk = welcome_func
 welcome_blk.call("felix") # => Welcome felix, join your 0 friend
 welcome_blk.call("estelle") # => Welcome estelle, join your 1 friend
 welcome_blk.call("cedric") # => Welcome estelle, join your 2 friends
+array=[]
 welcome_blk.call("fred") # => Welcome fred, join your 3 friends
